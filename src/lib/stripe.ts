bf5414cb81569d55fafdb6bbf2d1f4c5b48b25db
Lourenço <henri.lourenco@outlook.com>
2023-05-26 00:57:46 -0300

    [Add] Update stripe lib

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,6 +1,6 @@
 import Stripe from "stripe"
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
     apiVersion: "2022-11-15",
     appInfo: {
         name: "Ignite Shop",
